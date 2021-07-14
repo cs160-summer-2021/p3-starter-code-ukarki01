0d3ea0af750aa2cc15553e8beaa0ca8b2f906bcf
dariclim <limd212@gmail.com>
2021-07-14 01:39:50 -0700

    finished everything i think

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('index', views.index, name='index'),
+    path('new_interaction', views.new_interaction, name='new_interaction')
 ]
